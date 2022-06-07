57697f574d6fa3768b903105880544ccf928611b
istrav <travis.burandt@gmail.com>
2022-06-07 11:41:57 -0500

    itty and KV

--- clients/index.js
+++ clients/index.js
@@ -1,12 +1,63 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+import { Router } from 'itty-router'
+import {
+  json,
+  missing,
+  withContent,
+} from 'itty-router-extras'
+
+// now let's create a router (note the lack of "new")
+const router = Router()
+
+// GET collection index
+router.get('/', async () => {
+  const data = await CLIENTS.list();
+  let clients = JSON.stringify(data)
+
+  return handleRequest(clients)
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+
+// GET item
+router.get('/:id', async ({ params }) => {
+  const client = await CLIENTS.get(params.id)
+
+  return handleRequest(client)
+})
+
+// POST to the collection (we'll use async here)
+router.post('/:id', withContent, async ({ params, content}) => {
+  // save
+  content = JSON.stringify(content)
+  await CLIENTS.put(params.id, content)
+  
+  // refetch
+  const client = await CLIENTS.get(params.id)
+
+  return handleRequest(client)
+})
+
+router.delete('/:id', async ({ params }) => {
+  const client = await CLIENTS.delete(params.id)
+
+  return handleRequest(client)
+})
+
+// 404 for everything else
+router.all('*', () => new Response('Not Found.', { status: 404 }))
+
+// attach the router "handle" to the event handler
+addEventListener('fetch', event => {
+  event.respondWith(router.handle(event.request))
+})
+
+async function handleRequest(content, options) {
+  return new Response(content, {
+    ...options,
+    headers:  {
+      'content-type': 'application/json;charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,HEAD,OPTIONS',
+      'Access-Control-Allow-Headers': 'content-type',
+      'Access-Control-Max-Age': '86400',
+    },
   })
 }
