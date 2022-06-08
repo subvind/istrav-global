e59f34045684ab97008c0c6eb302fc8ae7c1ec73
istrav <travis.burandt@gmail.com>
2022-06-08 12:39:53 -0500

    clients 4

--- clients/index.js
+++ clients/index.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { Router } from 'itty-router'
 import {
   json,
@@ -5,53 +6,71 @@ import {
   withContent,
 } from 'itty-router-extras'
 
+// database collection
+import loki from 'lokijs'
+let db = new loki('istrav');
+let clients = db.addCollection('clients', { indices: ['id', 'email', 'firebaseAuthId'] });
+
+// grab clients from key value storage
+let data = await CLIENTS.get('all')
+data = JSON.parse(data)
+if (data && data.length) {
+  data.forEach((value) => {
+    // put client into collection one at a time
+    clients.insert(value)
+  })
+}
+
+// update database with in-memory records
+function save() {
+  let content = clients.find()
+  content = JSON.stringify(content)
+  await CLIENTS.put('all', content)
+}
+
 // now let's create a router (note the lack of "new")
 const router = Router()
 
 // GET collection index
 router.get('/', async () => {
-  const data = await CLIENTS.list();
-  let clients = JSON.stringify(data)
+  let clients = clients.find()
 
   return handleRequest(clients)
 })
 
 // GET item in collection
 router.get('/:id', async ({ params }) => {
-  const client = await CLIENTS.get(params.id)
+  let client = clients.findOne({ id: params.id })
 
   return handleRequest(client)
 })
 
 // POST new item to the collection
-router.post('/:id', withContent, async ({ params, content}) => {
-  // create
-  content = JSON.stringify(content)
-  await CLIENTS.put(params.id, content)
-  
-  // refetch
-  const client = await CLIENTS.get(params.id)
+router.post('/', withContent, async ({ params, content}) => {
+  content.id = uuidv4()
+  let client = clients.insert(content)
+  save()
 
   return handleRequest(client)
 })
 
 // UPDATE existing item in the collection
 router.put('/:id', withContent, async ({ params, content}) => {
-  // save
-  content = JSON.stringify(content)
-  await CLIENTS.put(params.id, content)
-  
-  // refetch
-  const client = await CLIENTS.get(params.id)
+  let client = clients.findOne({ id: params.id })
+  client.email = content.email || client.email
+  client.firebaseAuthId = content.firebaseAuthId || client.firebaseAuthId
+  clients.update(client)
+  save()
 
   return handleRequest(client)
 })
 
 // DELETE item from collection
 router.delete('/:id', async ({ params }) => {
-  const client = await CLIENTS.delete(params.id)
+  clients.remove({ id: params.id })
+  save()
 
-  return handleRequest(client)
+  return handleRequest(null)
 })
 
 // 404 for everything else
