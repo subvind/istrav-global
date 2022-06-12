76f6f3ccbdf4b9291d9d09bc53720a7069b46453
istrav <travis.burandt@gmail.com>
2022-06-11 19:52:44 -0500

    completely working 10

--- clients/index.js
+++ clients/index.js
@@ -94,7 +94,7 @@ async function verifyToken (content) {
 
   let isValid = jwt.verify(content, cert, { algorithm: 'RS256' })
   if (isValid) {
-    return jwt.decode(content).payload
+    return jsonwebtoken.decode(content)
   } else {
     return { error: true, message: 'Unable to verify token from firebase.' }
   }
